f1660c2b0b5df18aba72101bac67f7b42575ffdc
Saransh Gangwar <saransh7919@gmail.com>
2023-07-21 23:37:25 +0530

    first commit

--- src/pages/homepage/Testimonials.jsx
+++ src/pages/homepage/Testimonials.jsx
@@ -1,46 +1,46 @@
 import React from 'react'
-import Carousel from 'react-multi-carousel';
+// import Carousel from 'react-multi-carousel';
 import "react-multi-carousel/lib/styles.css"
-import { Testimonial1 } from '../../assets/images';
+// import { Testimonial1 } from '../../assets/images';
 
 const Testimonials = () => {
-    const responsive = {
-        desktop: {
-            breakpoint: { max: 3000, min: 1024 },
-            items: 1,
-        },
-        tablet: {
-            breakpoint: { max: 1024, min: 464 },
-            items: 1,
-        },
-        mobile: {
-            breakpoint: { max: 464, min: 0 },
-            items: 1,
-        }
-    };
-    const testimonials = [
-        {
-            id: 1,
-            image: Testimonial1,
-            name: 'Marvin McKinney',
-            about: 'Marketing Coordinator',
-            review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore '
-        },
-        {
-            id: 2,
-            image: Testimonial1,
-            name: 'Marvin McKinney',
-            about: 'Marketing Coordinator',
-            review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore '
-        },
-        {
-            id: 3,
-            image: Testimonial1,
-            name: 'Marvin McKinney',
-            about: 'Marketing Coordinator',
-            review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore '
-        },
-    ]
+    // const responsive = {
+    //     desktop: {
+    //         breakpoint: { max: 3000, min: 1024 },
+    //         items: 1,
+    //     },
+    //     tablet: {
+    //         breakpoint: { max: 1024, min: 464 },
+    //         items: 1,
+    //     },
+    //     mobile: {
+    //         breakpoint: { max: 464, min: 0 },
+    //         items: 1,
+    //     }
+    // };
+    // const testimonials = [
+    //     {
+    //         id: 1,
+    //         image: Testimonial1,
+    //         name: 'Marvin McKinney',
+    //         about: 'Marketing Coordinator',
+    //         review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore '
+    //     },
+    //     {
+    //         id: 2,
+    //         image: Testimonial1,
+    //         name: 'Marvin McKinney',
+    //         about: 'Marketing Coordinator',
+    //         review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore '
+    //     },
+    //     {
+    //         id: 3,
+    //         image: Testimonial1,
+    //         name: 'Marvin McKinney',
+    //         about: 'Marketing Coordinator',
+    //         review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore '
+    //     },
+    // ]
   return (
     <section className='testimonials'>
         <div className='heading'>What users says?</div>
